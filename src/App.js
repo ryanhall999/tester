09281281992a8c72aad8d8d7772bcc5c6e9efa54
Leighton A Pitman <leightonpitman14@gmail.com>
2020-01-22 21:24:15 -0500

    random 4

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
-import ColorSlider from "./color-slider/colorSliderApp";
-import { waitForDomChange } from "@testing-library/react";
-import ThisWeek from "./This-Week/thisWeek";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
+import ColorSlider from './color-slider/colorSliderApp';
+import { waitForDomChange } from '@testing-library/react';
+import ThisWeek from './This-Week/thisWeek';
 
 function App() {
-	return (
-		<Router>
-			<div>
-				<nav className="navbar bg-dark">
-					<Link className="nav-link" to="/color">
-						Color Slider
-					</Link>
-					<Link className="nav-link" to="/word">
-						The Word Game
-					</Link>
-				</nav>
+  return (
+    <Router>
+      <div>
+        <nav className="navbar bg-dark">
+          <Link className="nav-link" to="/color">
+            Color Slider
+          </Link>
+          <Link className="nav-link" to="/thisWeek">
+            The Word Game
+          </Link>
+        </nav>
 
-				<Switch>
-					<Route path="/color">
-						<ColorSlider />
-					</Route>
-					{/* <Route path="/inventory">
-						<InventoryGameApp />
-					</Route> */}
-				</Switch>
-			</div>
-		</Router>
-	);
+        <Switch>
+          <Route path="/color">
+            <ColorSlider />
+          </Route>
+          <Route path="/thisWeek">
+            <ThisWeek />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/This-Week/thisWeek.js
+++ src/This-Week/thisWeek.js
@@ -1,14 +1,44 @@
-import React, { useState } from "react";
-import moment from "moment";
-
-const now = moment().startOf("week");
-const week = [now];
-
-let i = 0;
-while (week.length < 7) {
-	week.push(moment(now).add("day", ++i));
-}
+import React, { useState } from 'react';
+import moment from 'moment';
+import './thisWeek.css';
 
 export default function ThisWeeek() {
-	return <div></div>;
+  const now = moment().startOf('week');
+  const initialWeek = [now];
+
+  let i = 0;
+  while (initialWeek.length < 7) {
+    initialWeek.push(moment(now).add('day', ++i));
+  }
+  const [week, setWeek] = useState(initialWeek);
+  const previous = () => {
+    let newWeek = week.map(day => {
+      return day.add(-1, 'week');
+    });
+    setWeek(newWeek);
+  };
+  const next = () => {
+    let newWeek = week.map(day => {
+      return day.add(1, 'week');
+    });
+    setWeek(newWeek);
+  };
+
+  return (
+    <div>
+      <div className="day-container">
+        {week.map((day, idx) => {
+          return (
+            <div id="days" key={idx}>
+              {day.format('dddd MMMM Do YYYY')}
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <button onClick={previous}>Previous Week</button>
+        <button onClick={next}>Next Week</button>
+      </div>
+    </div>
+  );
 }
