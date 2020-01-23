78a80b17720ddeb0a8968f0ec05c2f980bb917bf
Ryan Hall <ryanhall999@gmail.com>
2020-01-22 19:57:21 -0500

    2nd  commit

--- src/box.js
+++ src/box.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function Box() {
-	return <div></div>;
+export default function Box(props) {
+	console.log(props);
+	return (
+		<div
+			className="box"
+			style={{
+				backgroundColor: `rgb(${props.red},${props.green},${props.blue}) `
+			}}
+		></div>
+	);
 }
+
+// `rgb(${props.red}, ${props.green}, ${props.blue}) `
--- src/inputs.js
+++ src/inputs.js
@@ -1,38 +1,38 @@
-import React from 'react';
+import React from "react";
 
 export default function Inputs(props) {
-  return (
-    <div>
-      <div className="red">
-        <input
-          type="range"
-          min="1"
-          max="255"
-          value="50"
-          className="slider"
-          onChange={props.handleChange}
-        ></input>
-      </div>
-      <div className="green">
-        <input
-          type="range"
-          min="1"
-          max="255"
-          value="50"
-          className="slider"
-          onChange={props.handleChange}
-        ></input>
-      </div>
-      <div className="blue">
-        <input
-          type="range"
-          min="1"
-          max="255"
-          value="50"
-          className="slider"
-          onChange={props.handleChange}
-        ></input>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="red">
+				<input
+					type="range"
+					min="1"
+					max="255"
+					value={props.red}
+					className="slider"
+					onChange={props.handleChange}
+				></input>
+			</div>
+			<div className="green">
+				<input
+					type="range"
+					min="1"
+					max="255"
+					value={props.green}
+					className="slider"
+					onChange={props.handleChange}
+				></input>
+			</div>
+			<div className="blue">
+				<input
+					type="range"
+					min="1"
+					max="255"
+					value={props.blue}
+					className="slider"
+					onChange={props.handleChange}
+				></input>
+			</div>
+		</div>
+	);
 }
